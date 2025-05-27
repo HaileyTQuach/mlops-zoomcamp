4bc6dd84d161c5004b48332f2422de7c110192ff
HaileyQ <hailey.quach@ibm.com>
2025-05-27 00:31:39 +0000

    Updated sols

---
+++ 02-experiment-tracking/train.py
@@ -0,0 +1,47 @@
+import os
+import pickle
+import click
+import mlflow
+from sklearn.feature_extraction import DictVectorizer
+import   numpy as np
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    mlflow.sklearn.autolog()
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    with mlflow.start_run():
+        # Log parameters
+        mlflow.log_param("model_type", "RandomForestRegressor")
+        mlflow.log_param("max_depth", 10)
+        # Train the model
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
+        mlflow.log_metric("rmse", rmse)
+        mlflow.sklearn.log_model(rf, "model")
+        print(f"RMSE: {rmse}")
+
+
+if __name__ == '__main__':
+    run_train()
